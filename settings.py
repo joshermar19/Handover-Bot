72c3bc56d9c1a5da7b05d74c6d425c217ce5ba0d
joshermar19 <josh.martinez@bird.co>
2019-10-23 18:53:49 -0700

    changed formatting on HIGHPR_QUERY

--- settings.py
+++ settings.py
@@ -13,8 +13,7 @@ JIRA_PROJECT = 'NOC'  # Use 'NP' for DEBUG
 WHOOK_URL = os.environ.get('HANDOVER_WHOOK')
 
 HIGHPR_QUERY = ('project = NOC AND created > "-36h" AND '  # CHOSE APPROPRIATE TIME!
-                '(priority =  1 OR priority = 2) '
-                'ORDER BY key DESC')
+                '(priority =  1 OR priority = 2) ORDER BY key DESC')
 
 OUTSTD_QUERY = ('project = NOC AND type = Incident AND '
                 'status != Closed AND created >= "-7d" ORDER BY key DESC')
